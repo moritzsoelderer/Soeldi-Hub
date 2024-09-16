b6d29e185099b326e6303acb8d720947359dc5dc
moritzsoelderer <moritzsoelderer@gmail.com>
2024-09-16 21:28:24 +0200

    SHUB-11: Added caption to flow pojo and implemented controller logic for caption

--- src/main/java/soeldi/hub/soeldihub/SoeldiHubApplicationController.java
+++ src/main/java/soeldi/hub/soeldihub/SoeldiHubApplicationController.java
@@ -3,20 +3,15 @@ package soeldi.hub.soeldihub;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.*;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javafx.scene.media.MediaView;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import soeldi.hub.soeldihub.model.DatabaseService;
-import soeldi.hub.soeldihub.model.entities.Flow;
 import soeldi.hub.soeldihub.model.entities.Session;
 import soeldi.hub.soeldihub.model.entities.User;
 
-import java.util.Optional;
+import static soeldi.hub.soeldihub.utils.FlowPaneCreator.createFlowView;
 
 public class SoeldiHubApplicationController {
 
@@ -70,94 +65,11 @@ public class SoeldiHubApplicationController {
         //TODO implement behaviour
     }
 
-    private static StackPane createFlowMediaView(final Flow flow, final Region parentRegion) {
-        final double parentHeightRatio = 0.75;
-        final double nineToSixteenRatio = 9.0 / 16.0;
-
-        final Media media = new Media(flow.source().toString());
-        final MediaPlayer mediaPlayer = new MediaPlayer(media);
-        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
-
-        final StackPane parentStackPane = new StackPane();
-        parentStackPane.setStyle("-fx-background-radius: 20; -fx-background-color: #212121");
-        parentStackPane.maxWidthProperty()
-                .bind(parentRegion.heightProperty().multiply(parentHeightRatio * nineToSixteenRatio));
-        parentStackPane.maxHeightProperty()
-                .bind(parentRegion.heightProperty().multiply(parentHeightRatio));
-
-        final MediaView mediaView = new MediaView(mediaPlayer);
-        mediaView.setPreserveRatio(false);
-        mediaView.fitWidthProperty().bind(parentStackPane.maxWidthProperty());
-        mediaView.fitHeightProperty().bind(parentStackPane.maxHeightProperty());
-
-        final Rectangle rectangle = new Rectangle();
-        rectangle.heightProperty().bind(parentRegion.heightProperty().multiply(parentHeightRatio));
-        rectangle.widthProperty().bind(parentStackPane.widthProperty());
-        rectangle.setArcWidth(40);
-        rectangle.setArcHeight(40);
-
-        parentStackPane.setClip(rectangle);
-        parentStackPane.getChildren().add(mediaView);
-
-        final int userId =  Session.getInstance().orElseThrow().getUserId();
-        final boolean isLikedByUser = DatabaseService.getInstance().isLikedBy(userId, flow.id().orElseThrow());
-
-        final VBox likeButtonVbox = createLikeButtonVbox(
-                flow.id().orElseThrow(), flow.likes().orElse(0), isLikedByUser
-        );
-
-        final AnchorPane anchorPane = new AnchorPane();
-        anchorPane.setStyle("-fx-background-color: transparent;");
-        parentStackPane.getChildren().add(anchorPane);
-        AnchorPane.setBottomAnchor(likeButtonVbox, 20.0);
-        AnchorPane.setRightAnchor(likeButtonVbox, 20.0);
-        anchorPane.getChildren().add(likeButtonVbox);
-
-        anchorPane.setOnMouseClicked(e -> {
-            if(mediaPlayer.getStatus() == MediaPlayer.Status.PLAYING) {
-                mediaPlayer.pause();
-            }
-            else {
-                mediaPlayer.play();
-            }
-        });
-        return parentStackPane;
-    }
-
-    private static VBox createLikeButtonVbox(final int flowId, final int likes, final boolean isLikedByUser) {
-        final Label label = new Label(String.valueOf(likes));
-        label.getStyleClass().add("like-label");
-
-        final Button button = new Button("like");
-        button.getStyleClass().add("like-button");
-        button.setOnAction(e -> addLikeToFlow(flowId, label, button));
-        if(isLikedByUser) {
-            button.setStyle("-fx-background-color: red");
-        }
-
-        final VBox vBox = new VBox(button, label);
-        vBox.getStyleClass().add("like-vbox");
-
-        return vBox;
-    }
-
-    private static void addLikeToFlow(final int flowId, final Label likeLabel, final Button likeButton) {
-        Optional.of(DatabaseService.getInstance())
-                .flatMap(service -> service.createLike(Session.getInstance().orElseThrow().getUserId(), flowId))
-                .filter(bool -> bool)
-                .ifPresent(
-                        bool -> {
-                            likeLabel.setText(String.valueOf(Integer.parseInt(likeLabel.getText()) + 1));
-                            likeButton.setStyle("-fx-background-color: red;");
-                        }
-                );
-    }
-
     private void fillContentWithLatestFlows() {
         DatabaseService.getInstance().findLatestFlows().ifPresent(
                 flows -> flows.forEach(
                         optFlow -> optFlow.
-                                map(flow -> flowsVbox.getChildren().add(createFlowMediaView(flow, contentVbox)))
+                                map(flow -> flowsVbox.getChildren().add(createFlowView(flow, contentVbox)))
                 )
         );
     }
